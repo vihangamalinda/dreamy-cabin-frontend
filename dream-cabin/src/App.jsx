a224f3631f63ce2d2a35cc5a8a268c59647ae919
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-04 09:48:07 -0800

    feat: Building reuseable style-components.

--- dream-cabin/src/App.jsx
+++ dream-cabin/src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from './styles/GlobalStyle';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyleApp = styled.div`
-  background-color: orchid;
+  /* background-color: orchid; */
   padding: 20px;
 `;
 
@@ -14,13 +15,28 @@ function App() {
     <>
       <GlobalStyles />
       <StyleApp>
-        <Heading as="h1">Dream Cabin</Heading>
-        <Heading as="h2">Check In and Out</Heading>
-        <Button>Check IN</Button>
-        <Button>Check Out</Button>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">Dream Cabin</Heading>
+            <div>
+              <Heading as="h2">Check In and Out</Heading>
+              <Button variation="primary" size="medium">
+                Check IN
+              </Button>
+              <Button variation="secondary" size="small">
+                Check Out
+              </Button>
+            </div>
+          </Row>
 
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests" />
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyleApp>
     </>
   );
