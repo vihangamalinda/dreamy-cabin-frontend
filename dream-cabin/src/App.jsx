8174cba917f00675508912318b3c1c6dcfc237f0
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-03 17:30:00 -0800

    feat: Integrating the style component.

--- dream-cabin/src/App.jsx
+++ dream-cabin/src/App.jsx
@@ -1,5 +1,31 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyle';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const StyleApp = styled.div`
+  background-color: orchid;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Vihanga</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyleApp>
+        <H1>Dream Cabin</H1>
+        <Button>Check IN</Button>
+        <Button>Check Out</Button>
+
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyleApp>
+    </>
+  );
 }
 
 export default App;
--- dream-cabin/src/features/settings/UpdateSettingsForm.jsx
+++ dream-cabin/src/features/settings/UpdateSettingsForm.jsx
@@ -1,6 +1,6 @@
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
 
 function UpdateSettingsForm() {
   return (
