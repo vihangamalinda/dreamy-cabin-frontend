b8c24f0abbd9e6d746dbb74d43140eda2008b35b
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-04 17:13:56 -0800

    feat: Building AppLayout.

--- dream-cabin/src/App.jsx
+++ dream-cabin/src/App.jsx
@@ -8,6 +8,7 @@ import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyle';
+import AppLayout from './ui/AppLayout';
 
 // const StyleApp = styled.div`
 //   /* background-color: orchid; */
@@ -20,13 +21,16 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<DashBoard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<DashBoard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
