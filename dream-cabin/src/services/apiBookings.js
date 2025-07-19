36f95ff83003500ff125f829a9315c5fbdabf568
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-19 02:38:16 -0800

    feat: Booking related api calls.

--- dream-cabin/src/services/apiBookings.js
+++ dream-cabin/src/services/apiBookings.js
@@ -1,6 +1,16 @@
-import { getToday } from '../utils/helpers';
-import supabase from './supabase';
+// import { getToday } from '../utils/helpers';
+// import supabase from './supabase';
 
+import {
+  BOOKINGS,
+  PERFORM_CREATE,
+  PERFORM_GET_ALL,
+  PERFORM_GET_BY_ID,
+  PERFORM_UPDATE_BY_ID,
+  STATUS,
+} from './apiConfig';
+import { createData, getData, updateData } from './apiHelper';
+/*
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from('bookings')
@@ -95,3 +105,23 @@ export async function deleteBooking(id) {
   }
   return data;
 }
+
+*/
+
+const getAllBookings = async () => {
+  return await getData(BOOKINGS + PERFORM_GET_ALL);
+};
+
+const getBookingById = async (id) => {
+  return await getData(BOOKINGS + PERFORM_GET_BY_ID(id));
+};
+const createBooking = async (obj) => {
+  const url = BOOKINGS + PERFORM_CREATE;
+  return await createData({ url, obj });
+};
+const updateBookingStatus = async ({ id, status }) => {
+  const url = BOOKINGS + PERFORM_UPDATE_BY_ID(id) + STATUS;
+  return await updateData({ url, obj: { status,id, } });
+};
+
+export { getAllBookings, getBookingById, createBooking,updateBookingStatus };
