63cd8aba33b05a54c417c92af2c9bb9263a58053
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-19 02:37:11 -0800

    feat: API fetch calls with relavant method types.

---
+++ dream-cabin/src/services/apiHelper.js
@@ -0,0 +1,64 @@
+const getData = async (url) => {
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+    });
+    validateResponse(response);
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error', error);
+  }
+};
+
+const updateData = async ({ url, obj }) => {
+  try {
+    console.log(url)
+    const response = fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify(obj),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    validateResponse(response);
+    return await response.json();
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+const deleteData = async (url) => {
+  try {
+    const response = fetch(url, {
+      method: 'DELETE',
+    });
+    validateResponse(response);
+    return await response.json();
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+const createData = async ({ url, obj }) => {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(obj),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    validateResponse(response);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+const validateResponse = (response) => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+};
+export { getData, updateData, deleteData, createData };
