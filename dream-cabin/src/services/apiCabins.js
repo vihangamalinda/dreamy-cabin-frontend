d260ec650367131017c33bb18641b66482e073ac
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-22 09:53:16 -0800

    feat: Logic related to create cabin API call.

--- dream-cabin/src/services/apiCabins.js
+++ dream-cabin/src/services/apiCabins.js
@@ -1,10 +1,11 @@
 import {
   CABINS,
+  PERFORM_CREATE,
   PERFORM_DELETE,
   PERFORM_GET_ALL,
   PERFORM_GET_BY_ID,
 } from './apiConfig.js';
-import { getData, deleteData } from './apiHelper.js';
+import { getData, deleteData, createData } from './apiHelper.js';
 
 export async function getCabins() {
   return await getData(CABINS + PERFORM_GET_ALL);
@@ -16,4 +17,10 @@ export async function getCabinById(id) {
 
 export async function deleteById(id) {
   return await deleteData(CABINS + PERFORM_DELETE(id));
+}
+
+export async function createCabin(cabin) {
+  const obj = {...cabin,isActive:true};
+  const url = CABINS+PERFORM_CREATE;
+  await createData({obj,url})
 }
